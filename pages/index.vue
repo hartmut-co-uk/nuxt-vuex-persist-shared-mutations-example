46f4b51696af5444f18c7e443e6b65977ca1009b
Hartmut <info@hartmut.co.uk>
2020-05-01 17:47:57 +0200

    Adds instructions to index file (Live Preview)

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,26 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
+      <h1 class="title padded">
         nuxt-vuex-persist-shared-mutations-example
       </h1>
-      <h2 class="subtitle">
+      <h2 class="subtitle padded">
         My dazzling Nuxt.js project
       </h2>
-      <div class="main">
-        <h1>{{ $store.state.count }}</h1>
-        <button @click="$store.commit('decrement');">-</button>
-        <button @click="$store.commit('increment');">+</button>
+      <div class="main padded">
+        <div class="padded">
+          <h1>{{ $store.state.count }}</h1>
+          <button @click="$store.commit('decrement');">-</button>
+          <button @click="$store.commit('increment');">+</button>
+        </div>
+        <div class="padded">
+          <ol>
+            <li>Open this example in multiple tabs and play with (+), (-) buttons.</li>
+            <li>Reload page - and/or re-open in new tab/windows.</li>
+          </ol>
+        </div>
       </div>
-      <div class="links">
+      <div class="padded">
         <a
           href="https://github.com/hartmut-co-uk/nuxt-vuex-persist-shared-mutations-example"
           target="_blank"
@@ -61,14 +69,9 @@ export default {
   font-size: 20px;
   color: #526488;
   word-spacing: 5px;
+}
+
+.padded {
   padding-bottom: 15px;
-}
-
-.main {
-  padding: 10px;
-}
-
-.links {
-  padding: 10px;
 }
 </style>
